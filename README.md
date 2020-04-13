e9d7ccf63698e09119737588d94ed1973ebd5a35
morphisjustfun <mariojacobo2011@gmail.com>
2020-04-13 00:08:32 -0500

    Cambios finales

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# cs1103_2020_1_linked_list
-Ejercicio de Linked List
+## cs1103_2020_1_linked_list
+Ejercicio de linked list - Mario Jacobo Rios Gamboa - 201910285
---
+++ cmake-build-debug/CMakeFiles/poo2_2020_1_linked_list.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/poo2_2020_1_linked_list.dir/linked_list.cpp.o"
+  "CMakeFiles/poo2_2020_1_linked_list.dir/main.cpp.o"
+  "poo2_2020_1_linked_list"
+  "poo2_2020_1_linked_list.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/poo2_2020_1_linked_list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
